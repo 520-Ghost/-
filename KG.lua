9f6969e45dbf2043aa83bd6426f860ff1f247ded
鬼 <3065395172@qq.com>
2025-08-06 17:54:17 +0800

    Update KG.lua

--- KG.lua
+++ KG.lua
@@ -1,4 +1,4 @@
-setclipboard("https://qm.qq.com/q/fNPhz5pKh4")
+setclipboard("https://qm.qq.com/q/eJxmOnLJ3U")
 while wait(1) do
  game.Players.LocalPlayer:Kick("此脚本已经不能用了，如还想用kg脚本，已帮你复制kg新群，可获得最新脚本")
 end
